f886b5f9ff88561ee2031ae6d15e81ab6bfedcfd
Sebastian Jensen <apps@tsjdev-apps.de>
2024-06-23 19:01:59 +0200

    Add Statics containing prompts and static content.

---
+++ src/Phi3MiniOnnxConsole/Utils/Statics.cs
@@ -0,0 +1,20 @@
+﻿namespace Phi3MiniOnnxConsole.Utils;
+
+internal class Statics
+{
+    public const string SystemPrompt
+        = "You are an AI assistant that helps people find information. " +
+        "Answer the questions briefly.";
+
+    public const string ModelLoadingMessage
+        = "[yellow]Loading Model...[/]";
+
+    public const string ModelInputPrompt
+        = "Enter the path to the [yellow]model folder[/]:";
+
+    public const string InputPrompt =
+        "[green]Input:[/] ";
+
+    public const string OutputPrompt =
+        "[green]Output:[/]";
+}
