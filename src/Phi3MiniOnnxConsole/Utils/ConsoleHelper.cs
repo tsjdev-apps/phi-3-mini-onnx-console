fb0fd3262884f61620d94b7e5f4dfb8ffb4aa2de
Sebastian Jensen <apps@tsjdev-apps.de>
2024-06-23 19:01:44 +0200

    Add ConsoleHelper.

---
+++ src/Phi3MiniOnnxConsole/Utils/ConsoleHelper.cs
@@ -0,0 +1,84 @@
+﻿using Spectre.Console;
+
+namespace Phi3MiniOnnxConsole.Utils;
+
+internal static class ConsoleHelper
+{
+    /// <summary>
+    ///     Clears the console and creates the header for the application.
+    /// </summary>
+    public static void ShowHeader()
+    {
+        AnsiConsole.Clear();
+
+        Grid grid = new();
+        grid.AddColumn();
+        grid.AddRow(new FigletText("Phi-3 Mini ONNX").Centered().Color(Color.Red));
+        grid.AddRow(Align.Center(new Panel("[red]Sample by Thomas Sebastian Jensen ([link]https://www.tsjdev-apps.de[/])[/]")));
+
+        AnsiConsole.Write(grid);
+        AnsiConsole.WriteLine();
+    }
+
+    /// <summary>
+    ///     Gets the folder path from the user.
+    /// </summary>
+    /// <param name="prompt">The prompt message.</param>
+    /// <returns>The folder path entered by the user.</returns>
+    public static string GetFolderPath(string prompt)
+    {
+        ShowHeader();
+
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>(prompt)
+            .PromptStyle("white")
+            .ValidationErrorMessage("[red]Invalid path[/]")
+            .Validate(prompt =>
+            {
+                if (!Directory.Exists(prompt))
+                {
+                    return ValidationResult.Error("[red]Path does not exist[/]");
+                }
+
+                return ValidationResult.Success();
+            }));
+    }
+
+    /// <summary>
+    ///     Displays a prompt with the provided message and returns the user input.
+    /// </summary>
+    /// <param name="prompt">The prompt message.</param>
+    /// <returns>The user input.</returns>
+    public static string GetString(string prompt)
+    {
+        ShowHeader();
+
+        return AnsiConsole.Prompt(
+            new TextPrompt<string>(prompt)
+            .PromptStyle("white")
+            .ValidationErrorMessage("[red]Invalid prompt[/]")
+            .Validate(prompt =>
+            {
+                if (prompt.Length < 3)
+                {
+                    return ValidationResult.Error("[red]Value too short[/]");
+                }
+
+                if (prompt.Length > 200)
+                {
+                    return ValidationResult.Error("[red]Value too long[/]");
+                }
+
+                return ValidationResult.Success();
+            }));
+    }
+
+    /// <summary>
+    ///     Writes the specified text to the console.
+    /// </summary>
+    /// <param name="text">The text to write.</param>
+    public static void WriteToConsole(string text)
+    {
+        AnsiConsole.Markup($"[white]{text}[/]");
+    }
+}
